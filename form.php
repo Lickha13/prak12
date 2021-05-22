6940fbdb9d19db4da0ae5b1642a66362bef3ea6d
Lickha13 <lickhalavenia13@gmail.com>
2021-05-22 14:46:26 +0700

    prak12

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Pendaftaran</title>
+</head>
+<body>
+	<form method="post" action="simpan.php">
+		<table>
+			<tr><td>NAMA</td><td><input type="text" name="nama"></td></tr>
+			<tr><td>KELAS</td><td><input type="text" name="kelas"></td></tr>
+			<tr><td>ALAMAT</td><td><input type="text" name="alamat"></td></tr>
+			<tr><td colspan="2"><button type="submit" value="simpan">SIMPAN</button></td></tr>
+		</table>
+		</table>
+	</form>
+</body>
+</html>
